421ade5aad7dce1b65522ca60a29affd60fa1d71
Ryo Yoneyama (@yulii) <yone.info@gmail.com>
2015-11-23 21:24:17 +0900

    changed private method

--- lib/tanka/factory/base.rb
+++ lib/tanka/factory/base.rb
@@ -2,14 +2,16 @@ module Tanka
   class Factory
     class Base
 
-      def initialize(object, options)
-        @object = object
-        @null   = options[:null] || 0
-      end
-
       def build
         return nil if (1.0 - @null) < rand(0..1.0)
         @object
+      end
+
+      private
+
+      def initialize(object, options)
+        @object = object
+        @null   = options[:null] || 0
       end
     end
   end
